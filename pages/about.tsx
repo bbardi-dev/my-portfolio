8b7b859ba304562f892b430effd5d888f6354282
bbardi-dev <bbardi.dev@gmail.com>
2021-10-14 10:44:02 +0200

    initial commit

---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const about: NextPage = () => {
+  return (
+    <div>
+      <h1>About me</h1>
+    </div>
+  );
+};
+
+export default about;
---
+++ pages/projects.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const projects: NextPage = () => {
+  return (
+    <div>
+      <h1>My Projects</h1>
+    </div>
+  );
+};
+
+export default projects;
