c80f3bb16e431a3c08f578fff7ce49217be6034d
bbardi-dev <bbardi.dev@gmail.com>
2022-01-12 14:52:40 +0100

    contact section finished

--- components/sections/Contact.tsx
+++ components/sections/Contact.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { SubmitHandler, useForm } from "react-hook-form";
 import styles from "../../styles/components/sections/contact.module.scss";
 
@@ -23,45 +24,59 @@ export default function ContactForm() {
     <div className={styles.contact} id='contact'>
       <h2>Contact</h2>
 
-      <div className={styles.contact_form}>
-        <form onSubmit={handleSubmit(onFormSubmit)}>
-          <div className={styles.input}>
-            <label htmlFor='name'>Name</label>
-            <input type='text' {...register("name")} />
-          </div>
-          <div className={styles.input}>
-            <label htmlFor='email'>Email</label>
-            <input type='text' {...register("email")} />
-          </div>
-          <div className={styles.input}>
-            <label htmlFor='message'>Message</label>
-            <textarea rows={12} {...register("message")} />
-          </div>
-          <button type='submit'>
-            <span>Send Email</span>
-            <Image src={"/icons/send_it.svg"} width={20} height={20} />
-          </button>
-        </form>
-      </div>
+      <div className={styles.contact_content}>
+        <div className={styles.contact_form}>
+          <form onSubmit={handleSubmit(onFormSubmit)}>
+            <div className={styles.input}>
+              <label htmlFor='name'>Name</label>
+              <input type='text' {...register("name")} />
+            </div>
+            <div className={styles.input}>
+              <label htmlFor='email'>Email</label>
+              <input type='text' {...register("email")} />
+            </div>
+            <div className={styles.input}>
+              <label htmlFor='message'>Message</label>
+              <textarea rows={12} {...register("message")} />
+            </div>
+            <button type='submit'>
+              <span>Send Email</span>
+              <Image src={"/icons/send_it.svg"} width={40} height={40} />
+            </button>
+          </form>
+        </div>
 
-      <div className={styles.contact_info}>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo facere
-          similique temporibus. Nulla maxime vitae, sit assumenda deleniti nemo
-          dicta.
-        </p>
-        <div>
-          <Image src={"/icons/email.svg"} width={20} height={20} />
-          <span>example@example.com</span>
-        </div>
-        <div>
-          <Image src={"/icons/location.svg"} width={20} height={20} />
-          <span>Budapest, Remote</span>
-        </div>
-        <div>
-          <Image src={"/icons/github.svg"} width={20} height={20} />
-          <Image src={"/icons/linkedin.svg"} width={20} height={20} />
-          <Image src={"/icons/upwork.svg"} width={20} height={20} />
+        <div className={styles.contact_info}>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo facere
+            similique temporibus. Nulla maxime vitae, sit assumenda deleniti
+            nemo dicta.
+          </p>
+          <div className={styles.icon_info}>
+            <Image src={"/icons/email.svg"} width={36} height={36} />
+            <span>bbardi.dev@gmail.com</span>
+          </div>
+          <div className={styles.icon_info}>
+            <Image src={"/icons/location.svg"} width={36} height={36} />
+            <span>Budapest, Remote</span>
+          </div>
+          <div className={styles.socials}>
+            <Link href='https://www.github.com/bbardi-dev'>
+              <a target='blank'>
+                <Image src={"/icons/github.svg"} width={48} height={48} />
+              </a>
+            </Link>
+            <Link href='https://www.linkedin.com'>
+              <a target='blank'>
+                <Image src={"/icons/linkedin.svg"} width={48} height={48} />
+              </a>
+            </Link>
+            <Link href='https://www.upwork.com'>
+              <a target='blank'>
+                <Image src={"/icons/upwork.svg"} width={48} height={48} />
+              </a>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
